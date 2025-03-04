a570f1179101f5b595f9b859c5da62923643a1d3
Yansong Yang <yanyang@adobe.com>
2025-03-04 14:54:01 -0600

    no message

--- code/core/src/main/java/com/adobe/marketing/mobile/internal/eventhub/SDKDispatcher.kt
+++ code/core/src/main/java/com/adobe/marketing/mobile/internal/eventhub/SDKDispatcher.kt
@@ -9,22 +9,38 @@ import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.Executors
 
 // TODO: for testing only, will be using Dispatchers.IO.limitedParallelism() in the future
+@OptIn(ExperimentalCoroutinesApi::class)
 internal object SDKDispatcher {
     // ExtensionContainer.kt
-    internal val extensionEventDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-Ext")).asCoroutineDispatcher()
-    internal val subscriberDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-Ext")).asCoroutineDispatcher()
+//    internal val extensionEventDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-Ext")).asCoroutineDispatcher()
+
+//    internal val subscriberDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-Ext")).asCoroutineDispatcher()
+
     // NetworkService.kt
-    internal val networkThreadPoolDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-NS")).asCoroutineDispatcher()
+//    internal val networkThreadPoolDispatcher = Executors.newFixedThreadPool(2, CustomThreadFactory("ADB-NS")).asCoroutineDispatcher()
+
 
     // EventHub.kt
-    internal val eventHubDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-eventHubDispatcher")).asCoroutineDispatcher()
-    internal val eventDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-eventDispatcher")).asCoroutineDispatcher()
-    internal val completionHandlerDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-completionHandler")).asCoroutineDispatcher()
+//    internal val eventHubDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-eventHubDispatcher")).asCoroutineDispatcher()
+//    internal val eventDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-eventDispatcher")).asCoroutineDispatcher()
+//    internal val completionHandlerDispatcher = Executors.newFixedThreadPool(1, CustomThreadFactory("ADB-completionHandler")).asCoroutineDispatcher()
 
     //PersistentHitQueueV2.kt
-    private var queueCounter = 0;
+//    private var queueCounter = 0;
+//    fun createPersistQueueDispatcher(): CoroutineDispatcher {
+//        return Executors.newSingleThreadExecutor(CustomThreadFactory("ADB-PQ- ${queueCounter++}")).asCoroutineDispatcher()
+//    }
+
     fun createPersistQueueDispatcher(): CoroutineDispatcher {
-        return Executors.newSingleThreadExecutor(CustomThreadFactory("ADB-PQ- ${queueCounter++}")).asCoroutineDispatcher()
+        return Dispatchers.IO.limitedParallelism(1)
     }
+    internal val extensionEventDispatcher = Dispatchers.IO.limitedParallelism(2)
+    internal val subscriberDispatcher = Dispatchers.IO.limitedParallelism(2)
+    internal val networkThreadPoolDispatcher = Dispatchers.IO.limitedParallelism(32)
+
+
+    internal val eventHubDispatcher = Dispatchers.IO.limitedParallelism(1)
+    internal val eventDispatcher = Dispatchers.IO.limitedParallelism(1)
+    internal val completionHandlerDispatcher = Dispatchers.IO.limitedParallelism(1)
 
 }
